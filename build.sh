185e0bcd94d3b3eb305bba147619f610505e429f
Jay <jay@123.com>
2022-08-31 16:21:08 +0800

    try to fix one char result

--- build.sh
+++ build.sh
@@ -2,9 +2,13 @@
 
 mv ./.cargo/config.toml.bak ./.cargo/config.toml
 
+echo "build target x86_64-unknown-linux-musl"
 cargo build --release --target x86_64-unknown-linux-musl
+echo "build target x86_64-unknown-linux-gnu"
 cargo build --release --target x86_64-unknown-linux-gnu
+echo "build target aarch64-unknown-linux-gnu"
 cargo build --release --target aarch64-unknown-linux-gnu
+echo "build target x86_64-pc-windows-gnu"
 cargo build --release --target x86_64-pc-windows-gnu
 # run at the windows
 # cargo build --release --target x86_64-pc-windows-msvc
