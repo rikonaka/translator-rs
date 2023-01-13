3aa4f7d7f953223661d82af1d4c03029ab92c5da
Riko <xxy1836@gmail.com>
2023-01-13 11:09:26 +0800

    fix some bug

--- release.sh
+++ release.sh
@@ -2,12 +2,12 @@
 
 echo "DO NOT USE IT IN WINDOWS!!!"
 
-rm ./target/rust-translator-aarch64-unknown-linux-gnu.zip
-rm ./target/rust-translator-x86_64-pc-windows-gnu.zip
-rm ./target/rust-translator-x86_64-unknown-linux-gnu.zip
 rm ./target/rust-translator-x86_64-unknown-linux-musl.zip
+# rm ./target/rust-translator-x86_64-unknown-linux-gnu.zip
+# rm ./target/rust-translator-aarch64-unknown-linux-gnu.zip
+# rm ./target/rust-translator-x86_64-pc-windows-gnu.zip
 
-zip -j ./target/rust-translator-aarch64-unknown-linux-gnu.zip ./target/aarch64-unknown-linux-gnu/release/rust-translator
-zip -j ./target/rust-translator-x86_64-pc-windows-gnu.zip ./target/x86_64-pc-windows-gnu/release/rust-translator.exe
-zip -j ./target/rust-translator-x86_64-unknown-linux-gnu.zip ./target/x86_64-unknown-linux-gnu/release/rust-translator
 zip -j ./target/rust-translator-x86_64-unknown-linux-musl.zip ./target/x86_64-unknown-linux-musl/release/rust-translator
+# zip -j ./target/rust-translator-x86_64-unknown-linux-gnu.zip ./target/x86_64-unknown-linux-gnu/release/rust-translator
+# zip -j ./target/rust-translator-aarch64-unknown-linux-gnu.zip ./target/aarch64-unknown-linux-gnu/release/rust-translator
+# zip -j ./target/rust-translator-x86_64-pc-windows-gnu.zip ./target/x86_64-pc-windows-gnu/release/rust-translator.exe
--- src/main.rs
+++ src/main.rs
@@ -57,7 +57,7 @@ struct Args {
         long,
         value_parser,
         default_value = "0",
-        default_missing_value = "1"
+        default_missing_value = "0"
     )]
     clear: String,
     /// show original text or not
@@ -498,6 +498,11 @@ fn main() {
         _ => Some(args.proxy),
     };
     let clear_times: i32 = args.clear.parse().unwrap();
+    println!("c: {}", clear_times);
+    let clear_mode = match clear_times {
+        0 => false,
+        _ => true,
+    };
     // println!("clear: {}", clear_times);
     let no_original = match args.no_original.as_str() {
         "true" => true,
@@ -515,17 +520,19 @@ fn main() {
             thread::sleep(sleep_time);
             let selected_text = get_text();
             if selected_text.len() > 0 {
-                if sub_clear_times <= 0 {
-                    // send a control character to clear the terminal screen
-                    // print!("{}[2J", 27 as char);
-                    // set position the cursor at row 1, column 1
-                    print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
-                    sub_clear_times = clear_times;
+                if clear_mode {
+                    if sub_clear_times == 0 {
+                        // send a control character to clear the terminal screen
+                        // print!("{}[2J", 27 as char);
+                        // set position the cursor at row 1, column 1
+                        print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+                        sub_clear_times = clear_times;
+                    }
+                    sub_clear_times -= 1;
                 }
                 let translate_result = translate(&sl, &tl, &selected_text, index, &proxy_str);
                 translate_result.show(no_original, not_auto_break);
                 index += 1;
-                sub_clear_times -= 1;
             }
         }
     } else if cfg!(target_os = "windows") {
@@ -534,18 +541,20 @@ fn main() {
         loop {
             thread::sleep(sleep_time);
             let clipboard_text = get_text();
-            if sub_clear <= 0 {
-                // send a control character to clear the terminal screen
-                // print!("{}[2J", 27 as char);
-                // set position the cursor at row 1, column 1
-                // print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
-                sub_clear = clear_times;
-            }
             if clipboard_text.len() > 0 {
+                if clear_mode {
+                    if sub_clear == 0 {
+                        // send a control character to clear the terminal screen
+                        // print!("{}[2J", 27 as char);
+                        // set position the cursor at row 1, column 1
+                        // print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+                        sub_clear = clear_times;
+                    }
+                    sub_clear -= 1;
+                }
                 let translate_result = translate(&sl, &tl, &clipboard_text, index, &proxy_str);
                 translate_result.show(no_original, not_auto_break);
                 index += 1;
-                sub_clear -= 1;
             }
         }
     } else {
