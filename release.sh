289cd206bfe0d91e7c9c91feb2d9573049e6543d
Riko <xxy1836@gmail.com>
2023-10-19 01:06:58 +0800

    Fix release.sh

--- release.sh
+++ release.sh
@@ -2,12 +2,6 @@
 
 echo "DO NOT USE IT IN WINDOWS!!!"
 
-rm ./target/translator-rs-x86_64-unknown-linux-musl.zip
-# rm ./target/translator-rs-x86_64-unknown-linux-gnu.zip
-# rm ./target/translator-rs-aarch64-unknown-linux-gnu.zip
-# rm ./target/translator-rs-x86_64-pc-windows-gnu.zip
+rm ./translator-rs-x86_64-unknown-linux-musl.zip
 
-zip -j ./target/translator-rs-x86_64-unknown-linux-musl.zip ./target/x86_64-unknown-linux-musl/release/translator-rs
-# zip -j ./target/translator-rs-x86_64-unknown-linux-gnu.zip ./target/x86_64-unknown-linux-gnu/release/translator-rs
-# zip -j ./target/translator-rs-aarch64-unknown-linux-gnu.zip ./target/aarch64-unknown-linux-gnu/release/translator-rs
-# zip -j ./target/translator-rs-x86_64-pc-windows-gnu.zip ./target/x86_64-pc-windows-gnu/release/translator-rs.exe
+zip -j ./translator-rs-x86_64-unknown-linux-musl.zip ./target/x86_64-unknown-linux-musl/release/translator-rs
